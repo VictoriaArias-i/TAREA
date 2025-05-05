d57b87ff70fd9d9668b40e503a0d0d15a5930a9b
VictoriaArias-i <victoria.arias.me@gmail.com>
2025-05-05 10:46:40 -0400

    Add files via upload

---
+++ Clase_12.py
@@ -0,0 +1,14 @@
+#   Clase 05-05-2025
+
+"""Ejercicios de prueba"""
+
+#El supermercado acuenta necesita desarrollar un software en Python que le permita llevar la merma y las devoluciones. Dentro de las mermas vamos a considerar el 25% de perdida
+#del pollo, si el día miercoles recibimos 1000 kg de pollo, el dia jueves recibimos 1200 kg de pollo y el sabado solo 500 kg. Considermeos la merma de esa semana.
+
+#  Realicemos una lista de 10 productos que ingresará el cliente nosotros vamos a permitir la devolución de solo 3 productos y esos productos no deben superar el 20% total de
+#  todos los productos. 
+#   Mostrar en pantalla los productos que vamos a permitir devolver. Más un mensaje de aliento a subir las ventas.
+#   Opción para salir del programa.
+
+def inicio_prog():
+    print("")
