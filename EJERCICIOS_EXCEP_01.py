d31cba1c7ee7bb1b23a068e6c24d1e588cc6a8d6
VictoriaArias-i <victoria.arias.me@gmail.com>
2025-05-12 11:07:47 -0400

    Add files via upload

---
+++ EJERCICIOS_EXCEP_01.py
@@ -0,0 +1,27 @@
+"""EJERCICIO DE BUCLE UNO"""
+
+# Realiza construcción de un programa que deba realizar lo siguiente:
+# - Comienza con la inicialización de variables y solicita al usuario la cantidad de bultos. 
+# - Luego, utiliza un bucle FOR para procesar cada bulto, solicitando el peso al usuario y manejando posibles errores (agregar excepciones). 
+# Dependiendo del peso ingresado, acumula valores y contadores correspondientes para bultos livianos y normales. Finalmente, imprime el total a
+# pagar por bultos livianos y normales, así como la cantidad de bultos en cada categoría.
+
+# Una empresa de transporte requiere automatizar sus procesos de cálculo para poder cobrar por la cantidad de paquetes que trae un cliente.
+# Para calcular el valor total a cobrar y catalogarlo para envío, requiere preguntar el peso de cada bulto y determinar el valor según lo siguiente:
+
+# Ejemplo:
+# Si un cliente ingresa 3 bultos y según sus pesos estos clasifican en 1 liviano y 2 normales, el
+# cliente debe paga $5,000
+# El sistema debe mostrar lo siguiente:
+# 1 bulto liviano $1,000
+# 2 bultos normales $4,000
+# Valor total a pagar: $5,000
+
+"""EJERCICIO DE BUCLE DOS"""
+
+# Ahora, realiza la siguiente modificación al programa anterior:
+
+# - El bucle For, debe ser reemplazado por una sentencia While, que permita ejecutarse mientras la variable “nroBulto” sea menor o igual a la cantidad de bultos ingresados
+# por el usuario.
+# - Recuerda incluir sentencias de validación.
+
