b2dcc86114f8223783e213ca967e56d68a54ea4f
VictoriaArias-i <victoria.arias.me@gmail.com>
2025-06-06 10:50:54 -0400

    Add files via upload

---
+++ Clase_20.py
@@ -0,0 +1,28 @@
+"""CLASE 20     06-06-2025"""
+
+
+#   Git= Lenguaje con el que se trabaja Github, si se intala te deja trabajar con una consola(GitBash).
+#   Uni fue creado a la par con Uni.
+
+
+"""Ejercicio 01"""
+
+#La empresa Samsung nos acaba de contrar para realizar el directorio(almacen de datos, agenda) de su ultimo telefono
+#   el cual maneja su leguaje Python, nos solicita que esta aplicación almacene: Nombre, Apellido, Dirección, Cumpleaños y una Nota de 50 caracteres.
+#   esta agenda o lista de contacto debe poderse editar, guardar, modificar y eliminar (CRUD).
+#   Dicho esto resuelva lo que acabamos de pedir. 
+
+
+"""Ejercicio 02"""
+
+#La fundación DUOCUC solicita de su servicio profesional para desarrollar un sistema de cda uno de sus estudiantes como:
+# - Nombre
+# - Apellido
+# - ID
+# - Correo electronico 
+# - Carrera = Ingenieria, Analista y Gastronomia.
+# Ingenieria= Desarrollo web, Desarrollo movil, Desarrollo escritorio
+# Analista= Analisis de datos, Limpieza de datos, Creación Dashboard
+# Gastronomia= Historia de la gastronomia, Diferencia de alimentos naturales procesados, Sopaipilla 2
+
+#
