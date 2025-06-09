b727d01202d627184c1f9433b1285083c348d9fb
VictoriaArias-i <victoria.arias.me@gmail.com>
2025-06-09 10:47:17 -0400

    Add files via upload

---
+++ Clase_21.py
@@ -0,0 +1,23 @@
+"""Clase 21 09-06-25"""
+
+#   TAREA: Paso a paso para llegar a Git Push
+
+"""Ejercicio 01"""
+
+#   El hospital central de Stgo solicita ayuda para mejorar el ingreso a la sala de audiencia,
+#  y para ello requieren un software realizado en Python.
+#       Datos a solicitar del paciente:
+# 1-. N° de RUT, caracteristica del campo.
+#           Validar guión, que no exceda de 8 caracteres num, y valide el n° de ID.(Validar con try)
+# 2-. Nombre y apellido. (Por separado pero ambos se validan tipo de variable.)
+# 3-. Lista de patologia= Herida penetrante, Virus, Hemorragias, Reacciones alergicas o Deshidratación
+# 4-. Dirección. (Realizar una lista de comunas de la localidad. Si es que no esta en la lista prehecha tiene que salir
+#"Recurrir a su hospital de cercania")
+
+"""Ejercicio 02"""
+
+#   La empresa Netflix, solicita mejorar su menú de opciones de entretenimiento
+# 1-. Añadir peliculas.
+# 2-. Categorias= Acción, Ciencia ficcion, Terror, Comedia o salir de categorias.
+# 3-. Tiempo de duración de pelicula.
+# 4-. Salir.
